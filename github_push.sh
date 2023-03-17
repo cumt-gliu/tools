aedfaa0af271ab78d1238ddf1f16398f793db37c
Giannis <giannis.liu@nio.com>
2023-03-17 17:10:09 +0800

    atuo commit from [giannis.liu]

--- github_push.sh
+++ github_push.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
-bash_path=$1
-
+bash_path="/Users/giannis.liu/development/github"
+# 提交信息
 commit_message="atuo commit from ["$USER"]"
-echo $commit_message
 
+#  遍历bash_path下的git仓库
 for file in $bash_path/*; do
-	# 判断是否是目录并且是git仓库
+    # 判断是否是目录并且是git仓库
     if test -d $file && test -d "$file/.git/"
     then
-    	# 进入仓库执行git命令
-    	cd $file
-    	# 获取当前分支
-    	current_branch=`git branch --show-current`
-    	echo "path = $file + current_branch = $current_branch"
-    	git pull --rebase origin $current_branch
-    	echo "pull origin branch success!"
-    	git add .
-    	git commit -m "$commit_message"
-    	git push origin $current_branch
-    	echo "push ${current_branch} to remote success!"
-    	cd -
+        # 进入仓库执行git命令
+        cd $file
+        # 获取当前分支
+        current_branch=`git branch --show-current`
+        echo "path = $file + current_branch = $current_branch"
+        git pull --rebase origin $current_branch
+        echo "pull origin branch success!"
+        git add .
+        git commit -m "$commit_message"
+        git push origin $current_branch
+        echo "push ${current_branch} to remote success!"
+        cd -
     else
-    	echo $file 不是git仓库
+        echo $file 不是git仓库
     fi
 done
 
